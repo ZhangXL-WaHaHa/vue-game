621b83e8360d6d68e6d7d96455e7b5ca68e7773a
ZhangXL-WaHaHa <2263378161@qq.com>
2022-03-28 21:06:13 +0800

    fix: 测试

--- src/views/game/greedySnake/index.vue
+++ src/views/game/greedySnake/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <router-link to="playGreedySnake">
-      <button>开始游戏</button>
+      <button>准备开始游戏了吗</button>
     </router-link>
   </div>
 </template>
